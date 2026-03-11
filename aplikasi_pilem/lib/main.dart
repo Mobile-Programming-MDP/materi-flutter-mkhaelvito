65fbe396de9d09f699c7d760686a95ef5b0b0619
mkhaelvito <mikvit01@gmail.com>
2026-03-11 13:40:14 +0700

    update navbar

--- aplikasi_pilem/lib/main.dart
+++ aplikasi_pilem/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:aplikasi_pilem/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:aplikasi_pilem/screens/favorite_screen.dart';
+import 'package:aplikasi_pilem/screens/home_screen.dart';
+import 'package:aplikasi_pilem/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +10,64 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pilem',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+        ],
+      ),
     );
   }
 }
